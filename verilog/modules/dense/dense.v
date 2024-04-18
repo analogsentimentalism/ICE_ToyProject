f26bde44fa462c75062c0b11486de78710664618
YJ <cjdemforhs@g.skku.edu>
2024-04-18 18:40:44 +0900

    [Edit] Dense

--- verilog/modules/dense/dense.v
+++ verilog/modules/dense/dense.v
@@ -1,44 +1,49 @@
 module dense #(
-	parameter	W 			= 96	,
-	parameter	H 			= 96	,
-	parameter	BIAS 		= 256	,
-	parameter	DATA_WIDTH	= 32
-
+	parameter	NUMS		= 96 * 96 * 32			,	// 전체 원소 개수
+	parameter	BIAS 		= 256					,
+	parameter	DATA_WIDTH	= 32					,
+	parameter	BIASFILE	= "dense_bias.txt"		,
+	parameter	KERNELFILE	= "dense_kernel.txt"
 ) (
 	input								clk				,
-	input	[DATA_WIDTH * W * H -1:0]	data_i			,
-	input	[DATA_WIDTH * BIAS-1:0]		bias_i			,
-	input	[DATA_WIDTH * W * H -1:0]	kernal_i		,
+	input	[DATA_WIDTH * NUMS -1:0]	data_i			,
 	output	[DATA_WIDTH * BIAS-1:0]		result_o
 );
 
-wire	[DATA_WIDTH-1:0]			temp		[0:2*W*H-2]	;
-wire	[DATA_WIDTH-1:0]			bias_result	[0:BIAS-1]	;
+wire	[DATA_WIDTH-1:0]	temp		[0:2*NUMS-2]	;
+wire	[DATA_WIDTH-1:0]	bias_result	[0:BIAS-1]	;
+
+reg		[DATA_WIDTH-1:0]	bias		[0:BIAS-1]	;
+reg		[DATA_WIDTH-1:0]	kernel		[0:NUMS-1]	;
+
+initial begin
+	$readmemh(	BIASFILE,	bias	);
+	$readmemh(	KERNELFILE,	kernel	);
+end
+
 genvar i, j, k, l;
 generate
-	for(i=0;i<H;i=i+1) begin: multi
-		for(j=0;j<W;j=j+1) begin
-			FloatingMultiplication multiplication (
-				.A		(	data_i		[(i*H+j)*DATA_WIDTH+:DATA_WIDTH]		),
-				.B		(	kernal_i	[(i*H+j)*DATA_WIDTH+:DATA_WIDTH]		),
-				.clk	(	clk									),
-				.result	(	temp		[i*H+j]					)
-			);
-		end
+	for(i=0;i<NUMS;i=i+1) begin: multi_block
+		FloatingMultiplication multiplication (
+			.A		(	data_i	[i*DATA_WIDTH+:DATA_WIDTH]	),
+			.B		(	kernel	[i]							),
+			.clk	(	clk									),
+			.result	(	temp	[i]							)
+		);
 	end
-	for(k=0;k<2*W*H-1;k=k+2) begin: addition
+	for(k=0;k<2*NUMS-1;k=k+2) begin: addition_block
 		FloatingAddition addition (
-			.A		(	temp	[k]	),
-			.B		(	temp	[k + 1]	),
-			.result	(	temp	[W*H+k/2]	)
+			.A		(	temp	[k]			),
+			.B		(	temp	[k + 1]		),
+			.result	(	temp	[NUMS+k/2]	)
 		);
 	end
 
-	for(l=0;l<BIAS;l=l+1) begin: bias
+	for(l=0;l<BIAS;l=l+1) begin: bias_block
 		FloatingAddition bias_adder (
-			.A		(	temp		[2*W*H-2]					),
-			.B		(	bias_i		[l*DATA_WIDTH+:DATA_WIDTH]	),
-			.result	(	bias_result	[l]							)
+			.A		(	temp		[2*NUMS-2]	),
+			.B		(	bias		[l]			),
+			.result	(	bias_result	[l]			)
 		);
 
 		relu #(
--- verilog/testbenches/dense/dense_tb.v
+++ verilog/testbenches/dense/dense_tb.v
@@ -1,7 +1,7 @@
 module dense_tb #(
-	parameter 	W			= 8,
-	parameter	H			= 8,
-	parameter	BIAS		= 32,
+	parameter 	W			= 3,	// NUMS로 고쳐야하지만 귀찮아서 걍...
+	parameter	H			= 1,
+	parameter	BIAS		= 256,
 	parameter	DATA_WIDTH	= 32
 ) ();
 
@@ -12,15 +12,12 @@ reg	[W*H*DATA_WIDTH-1:0]	kernal_i	;
 wire[BIAS*DATA_WIDTH-1:0]	result_o	;
 
 dense #(
-	.W			(	W			),
-	.H			(	H			),
+	.NUMS		(	W*H			),
 	.BIAS		(	BIAS		),
 	.DATA_WIDTH	(	DATA_WIDTH	)
 ) u_dense (
 	.clk		(	clk			),
 	.data_i		(	data_i		),
-	.bias_i		(	bias_i		),
-	.kernal_i	(	kernal_i	),
 	.result_o	(	result_o	)
 );
 
@@ -34,10 +31,6 @@ initial begin
 	clk	= 1'b1	;
 	for(i=0;i<W*H;i=i+1) begin
 		data_i	[i*DATA_WIDTH+:DATA_WIDTH]	= $shortrealtobits(1.5 + i * 0.03)	;
-		kernal_i[i*DATA_WIDTH+:DATA_WIDTH]	= $shortrealtobits(7.32 - i * 0.02)	;
-	end
-	for(i=0;i<BIAS;i=i+1) begin
-		bias_i	[i*DATA_WIDTH+:DATA_WIDTH]	= $shortrealtobits(8.92 - i * 0.02)	;
 	end
 	
 	for(i=0;i<BIAS;i=i+1) begin
