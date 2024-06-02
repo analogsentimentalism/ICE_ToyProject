3539bdca6236ce9c5dd4eae11801a40894b2e7d1
kmgkmgkmg <163232142+kmgkmgkmg@users.noreply.github.com>
2024-06-02 21:53:02 +0900

    Create processingElement.v

---
+++ verilog/modules/conv/int/processingElement.v
@@ -0,0 +1,26 @@
+`timescale 1 ns / 10 ps
+
+module processingElement(clk,reset,floatA,floatB,result);
+
+parameter input_DATA_WIDTH = 8;
+parameter output_DATA_WIDTH = 32;
+
+input clk, reset;
+input [input_DATA_WIDTH-1:0] floatA, floatB;
+output reg signed [output_DATA_WIDTH-1:0] result;
+
+wire signed [output_DATA_WIDTH-1:0] multResult;
+wire signed[output_DATA_WIDTH-1:0] addResult;
+
+floatMult FM (floatA,floatB,multResult);
+floatAdd FADD (multResult,result,addResult);
+
+always @ (posedge clk or posedge reset) begin
+	if (reset == 1'b1) begin
+		result = 0;
+	end else begin
+		result = addResult;
+	end
+end
+
+endmodule
