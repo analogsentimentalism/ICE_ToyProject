46873139516408b248db1480ef4aa9fdcbfbfd4a
kang0029 <rkdtnalsdd@naver.com>
2024-06-06 10:31:56 +0900

    [edit]

--- sources_1/imports/ICE_ToyProject/verilog/modules/line_3_buffer copy.v
+++ sources_1/imports/ICE_ToyProject/verilog/modules/line_3_buffer copy.v
@@ -9,9 +9,9 @@ parameter K = 6;
 input clk, resetn;
 input [D*W*DATA_BITS*K-1:0] input_data;
 input behind_conv_done;
-output [D*(W+2)*DATA_BITS-1:0] output_1;
-output [D*(W+2)*DATA_BITS-1:0] output_2;
-output [D*(W+2)*DATA_BITS-1:0] output_3;
+output [D*(W+2)*DATA_BITS*K-1:0] output_1;
+output [D*(W+2)*DATA_BITS*K-1:0] output_2;
+output [D*(W+2)*DATA_BITS*K-1:0] output_3;
 input valid_i;
 output valid_o;
 reg valid;
@@ -20,33 +20,20 @@ reg [D*(W+2)*DATA_BITS*K-1:0] buffer [2:0];
 reg [1:0] buffer_full;
 reg [1:0] counter;
 wire [1:0] ptr1, ptr2, ptr3;
-reg [2:0] out_counter;
 
 assign ptr1 = counter;
 assign ptr2 = (counter<2'b10) ? counter + 2'h1 : 2'b00;
 assign ptr3 = (ptr2<2'b10) ? ptr2+3'h1 : 2'b00;   
-assign output_1 = buffer[ptr1][D*(W+2)*DATA_BITS*out_counter +: D*(W+2)*DATA_BITS];
-assign output_2 = buffer[ptr2][D*(W+2)*DATA_BITS*out_counter +: D*(W+2)*DATA_BITS];
-assign output_3 = buffer[ptr3][D*(W+2)*DATA_BITS*out_counter +: D*(W+2)*DATA_BITS];
+assign output_1 = buffer[ptr1];
+assign output_2 = buffer[ptr2];
+assign output_3 = buffer[ptr3];
 assign valid_o = valid;
-
-always@ (posedge clk or negedge resetn) begin
-    if(!resetn)
-        out_counter <= 3'h0;
-    else if(behind_conv_done)
-        out_counter <= out_counter + 3'b001;
-    else if (out_counter == K)
-        out_counter <= 3'h0;
-    else
-        out_counter <= out_counter;
-end
-
 always@(posedge clk or negedge resetn) begin
     if(!resetn)
         zero_padding_counter <= 5'h0;
     else if(valid_i)
         zero_padding_counter <= zero_padding_counter + 5'h1;
-    else if ((zero_padding_counter == H) & behind_conv_done & (out_counter == K))
+    else if ((zero_padding_counter == H) & behind_conv_done)
         zero_padding_counter <= 5'h0;
 end
 always @ (posedge clk or negedge resetn) begin
@@ -54,7 +41,7 @@ always @ (posedge clk or negedge resetn) begin
         valid <= 1'b0;
     else if (valid)
         valid<= 1'b0;
-    else if ((valid_i & (buffer_full == 2'b01)) | behind_conv_done)
+    else if ((valid_i & (buffer_full >= 2'b01)) | ((zero_padding_counter == H)&behind_conv_done))
         valid<=1'b1;
 end
 always @(posedge clk or negedge resetn)begin
@@ -62,8 +49,8 @@ always @(posedge clk or negedge resetn)begin
         counter <= 2'h0;
     else if ((counter == 2'b10)&(valid_i | ((zero_padding_counter == H) & behind_conv_done)))
         counter <= 2'h0;
-    else if(valid_i|((zero_padding_counter == H)& behind_conv_done)&(out_counter == K))
-        counter <= counter + 2'b1;
+    else if(valid_i|((zero_padding_counter == H)& behind_conv_done))
+        counter <= counter+ + 2'b1;
 end
 always@(posedge clk or negedge resetn) begin
     if (!resetn)
